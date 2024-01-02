5933678260ac0d7ab1f2d1deca3d0e8f83e6bfed
AungPhyoLwin <aungphyolwin@gicjp.com>
2024-01-02 16:02:11 +0630

    learn route controller view and install ui

---
+++ resources/views/test.blade.php
@@ -0,0 +1,5 @@
+<h1>Hello World</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa atque libero ab quaerat quod quos,
+     blanditiis culpa nisi mollitia magnam possimus quae tempore amet asperiores facere incidunt odio 
+     reprehenderit eaque sequi laborum nostrum consequatur nobis quia! Iste, vitae, molestiae temporibus 
+     ea soluta earum vel consequatur nesciunt minima officiis maiores molestias?</p>
